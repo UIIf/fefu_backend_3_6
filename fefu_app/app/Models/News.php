8b1e9396f21a57945ee764958ccb0e9d092e642a
Uiif <uiif.github@gmail.com>
2022-03-30 23:33:56 +1000

    Lesson 3: News api

---
+++ fefu_app/app/Models/News.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    use HasFactory, Sluggable;
+
+    protected $dates = ['published_at',];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+}
